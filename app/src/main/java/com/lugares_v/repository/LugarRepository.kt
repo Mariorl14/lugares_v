0fdcbd8d2224ce91730b6178a17c38a4ed295277
Mariorl14 <mariorl040301@gmaiol.com>
2022-10-05 23:21:15 -0600

    Semana 3

--- app/src/main/java/com/lugares_v/repository/LugarRepository.kt
+++ app/src/main/java/com/lugares_v/repository/LugarRepository.kt
@@ -4,7 +4,12 @@ import androidx.lifecycle.LiveData
 import com.lugares_v.data.LugarDao
 import com.lugares_v.model.Lugar
 class LugarRepository(private val lugarDao: LugarDao) {
-    suspend fun addLugar(lugar: Lugar) {
+    suspend fun saveLugar(lugar: Lugar) {
+        if (lugar.id == 0){
+            lugarDao.addLugar(lugar)
+        }else {
+            lugarDao.updateLugar(lugar)
+        }
         lugarDao.addLugar(lugar)
     }
     suspend fun updateLugar(lugar: Lugar) {
